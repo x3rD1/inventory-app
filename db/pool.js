4bc4bcd8b7ba596279646eb33bc9d09bcfd70eea
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-31 13:32:08 -0700

    Create pool.js

---
+++ db/pool.js
@@ -0,0 +1,9 @@
+const { Pool } = require("pg");
+require("dotenv").config();
+
+module.exports = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
