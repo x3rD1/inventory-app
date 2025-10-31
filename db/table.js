cab7f557403f022ee08d129d169bafd5872443e5
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-31 14:26:35 -0700

    Create table.js script to create 2 tables

---
+++ db/table.js
@@ -0,0 +1,36 @@
+const { Client } = require("pg");
+require("dotenv").config();
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS genres (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        name TEXT NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS animes (
+        id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        title TEXT NOT NULL,
+        genre_id INT REFERENCES genres(id)
+    );
+`;
+
+async function main() {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  try {
+    await client.connect();
+    await client.query(SQL);
+    console.log("Tables successfully created!");
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+    console.log("DONE!");
+  }
+}
+
+main();
