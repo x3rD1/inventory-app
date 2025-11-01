0476227ab225be4f487032ab4cf7dfed10b329a0
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-31 23:23:03 -0700

    Install express-session and add a session config in app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const path = require("node:path");
+require("dotenv").config();
 // Routers
 const indexRouter = require("./routes/indexRouter");
 const homeRouter = require("./routes/homeRouter");
+const loginRouter = require("./routes/loginRouter");
 // This Access CSS files
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
@@ -12,9 +15,20 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
+
+// Session Config
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "fallback",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 },
+  })
+);
 // Routes
 app.use("/", indexRouter);
 app.use("/home", homeRouter);
+app.use("/login", loginRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
