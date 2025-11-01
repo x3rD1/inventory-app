049cfca817af3368b71ccfa7662d1669333302a3
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-31 21:50:25 -0700

    Create ejs and css file for home

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+// Routers
 const indexRouter = require("./routes/indexRouter");
+const homeRouter = require("./routes/homeRouter");
 // This Access CSS files
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
@@ -10,8 +12,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
-// Main Router
+// Routes
 app.use("/", indexRouter);
+app.use("/home", homeRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
