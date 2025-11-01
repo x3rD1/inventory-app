c1a83e5b4c22b870454f51570aa92cfe06ebb971
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-10-31 21:08:08 -0700

    Add router for search method GET

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
-indexRouter.get("/", indexController.test);
+indexRouter.get("/", indexController.index);
+indexRouter.get("/search", indexController.search);
 
 module.exports = indexRouter;
