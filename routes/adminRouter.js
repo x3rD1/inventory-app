bcf288aef1bada7b779ebcc0618d18ed2b5aab8a
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-01 19:06:13 -0700

    Make routes for admin and for add/delete posts

---
+++ routes/adminRouter.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+const adminRouter = Router();
+const adminController = require("../controllers/adminController");
+
+adminRouter.post("/", adminController.adminPost);
+adminRouter.get("/room", adminController.adminPage);
+adminRouter.post("/room/add", adminController.addAnime);
+adminRouter.post("/room/delete/:id", adminController.deleteAnime);
+
+module.exports = adminRouter;
