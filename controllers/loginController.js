1ecbd0f4607fc4d8c6ff8f78ebb0bda5d71fd95a
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-01 19:01:45 -0700

    Update redirect and render links

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,25 +2,11 @@ require("dotenv").config();
 
 exports.login = (req, res) => {
   if (req.session.isAdmin) {
-    return res.redirect("/login/admin");
+    return res.redirect("/admin/room");
   }
-  res.render("login", { title: "Animeventory — Login", css: "login.css" });
-};
-
-exports.adminPost = (req, res) => {
-  const password = req.body.password;
-  if (password !== process.env.ADMIN_PASS) {
-    return res.redirect("/login");
-  }
-
-  req.session.isAdmin = true;
-  res.redirect("/login/admin");
-};
-
-exports.adminPage = (req, res) => {
-  if (!req.session.isAdmin) {
-    return res.redirect("/login");
-  }
-
-  res.render("admin", { title: "Animeventory — Admin", css: "admin.css" });
+  res.render("login", {
+    title: "Animeventory — Login",
+    css: "login.css",
+    script: "",
+  });
 };
