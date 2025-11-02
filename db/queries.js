c53437ce849b4bc06b4d926e738936768254e917
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-01 19:05:25 -0700

    Create queries for selecting, adding, deleting animes

---
+++ db/queries.js
@@ -0,0 +1,41 @@
+const pool = require("./pool");
+
+exports.add = async (title, genres) => {
+  const placeholders = genres.map((_, i) => `$${i + 1}`).join(", ");
+  const { rows } = await pool.query(
+    `SELECT id FROM genres WHERE name IN (${placeholders})`,
+    genres
+  );
+  const genreIds = rows.map((row) => row.id); //[1,2,3]
+
+  const { rows: animeRows } = await pool.query(
+    "INSERT INTO animes (title) VALUES ($1) RETURNING id",
+    [title]
+  );
+  const animeId = animeRows[0].id; // rows: [{ id: 1}]
+
+  // Uses genreIds length to create placeholders
+  const values = genreIds.map((_, i) => `($1, $${i + 2})`).join(", ");
+
+  await pool.query(
+    `INSERT INTO anime_genres (anime_id, genre_id) VALUES ${values}`, // ($1, $2), ($1, $3)
+    [animeId, ...genreIds] // [1, 2, 3, 4, 5, ...]
+  );
+};
+
+exports.showAnimeList = async () => {
+  const { rows } = await pool.query(
+    // TODO select id
+    "SELECT animes.id, animes.title, STRING_AGG(genres.name, ', ') AS genres FROM animes JOIN anime_genres ON animes.id = anime_genres.anime_id JOIN genres ON genres.id = anime_genres.genre_id GROUP BY animes.id, animes.title;"
+  );
+  return rows;
+};
+
+exports.delete = async (animeId) => {
+  try {
+    await pool.query("DELETE FROM anime_genres WHERE anime_id = $1", [animeId]);
+    await pool.query("DELETE FROM animes WHERE id = $1", [animeId]);
+  } catch (error) {
+    console.log(error);
+  }
+};
