180a8f32df528004941700c515b17d2b89659944
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-03 12:24:37 -0800

    Add a route for adding anime in home page

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -3,5 +3,6 @@ const homeRouter = Router();
 const homeController = require("../controllers/homeController");
 
 homeRouter.get("/", homeController.home);
+homeRouter.post("/add", homeController.addAnime);
 
 module.exports = homeRouter;
