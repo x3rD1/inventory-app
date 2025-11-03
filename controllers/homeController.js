f2779c1f30b8fd8fe8c14ac37e9d3bfd6b369374
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-02 23:57:34 -0800

    Add animeList from db

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,11 @@
-exports.home = (req, res) => {
+const db = require("../db/queries");
+
+exports.home = async (req, res) => {
+  const animeList = await db.showAnimeList();
   res.render("home", {
     title: "Animeventory — Homepage ",
     css: "home.css",
-    script: "",
+    script: "AddAnimeModal.js",
+    animes: animeList,
   });
 };
