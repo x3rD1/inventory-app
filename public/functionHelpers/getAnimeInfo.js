c24d8bf109cf34f3d096a9ee44c615b3cf29b9c8
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-03 12:25:59 -0800

    Move and delete some files

---
+++ public/functionHelpers/getAnimeInfo.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+
+const fileContent = fs.readFileSync("redirectedLinks.txt", "utf-8");
+const links = fileContent.split("\n").filter(Boolean);
+
+async function image(title) {
+  try {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(title)}`
+    );
+    const data = await response.json();
+    // If anime is One Piece
+    if (title.toLowerCase().replace(/\s+/g, "-") === "one-piece") {
+      const anime = data.data[1];
+      return (
+        anime.images?.webp?.large_image_url ||
+        anime.images?.jpg?.large_image_url ||
+        null
+      );
+    }
+
+    const anime = data.data[0];
+
+    if (!anime) return null;
+
+    return (
+      anime.images?.webp?.large_image_url ||
+      anime.images?.jpg?.large_image_url ||
+      null
+    );
+  } catch (error) {
+    console.error("Error fetching anime:", error);
+    return null;
+  }
+}
+
+function link(title) {
+  const kebabTitle = title.toLowerCase().replace(/\s+/g, "-");
+
+  const matchedLink = links.find((link) => link.includes(kebabTitle));
+  return matchedLink;
+}
+
+async function title(title) {
+  try {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(title)}`
+    );
+    const data = await response.json();
+    // if anime is One Piece
+    const kebabTitle = title.toLowerCase().replace(/\s+/g, "-");
+    if (kebabTitle === "one-piece") {
+      return data.data[1].title_english;
+    }
+    return data.data[0].title_english;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+module.exports = { image, link, title };
