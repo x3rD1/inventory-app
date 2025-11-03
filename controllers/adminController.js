553b732ce3cc456efb80fdd6cfb648a66b51aaa7
Rexx Alfaro <rexxalfaro+github@gmail.com>
2025-11-02 21:55:24 -0800

    Add update feature, and add fetching links and image

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 const db = require("../db/queries");
+const fs = require("fs");
+
+const fileContent = fs.readFileSync("redirectedLinks.txt", "utf-8");
+const links = fileContent.split("\n").filter(Boolean);
 
 exports.adminPost = (req, res) => {
   const password = req.body.password;
@@ -26,9 +30,20 @@ exports.adminPage = async (req, res) => {
 };
 
 exports.addAnime = async (req, res) => {
-  const { title, genre, link } = req.body;
+  const { title, genre } = req.body;
+  const image = (await getAnimeImage(title)) || null;
+  const link = getAnimeLink(title);
+  await db.add(title, genre, link, image);
+  res.redirect("/admin/room");
+};
 
-  await db.add(title, genre, link);
+exports.updateAnime = async (req, res) => {
+  const animeId = req.params.id;
+  const { title, genre } = req.body;
+  const image = (await getAnimeImage(title)) || null;
+  const link = getAnimeLink(title);
+  await db.update(title, link, genre, image, animeId);
+
   res.redirect("/admin/room");
 };
 
@@ -39,3 +54,31 @@ exports.deleteAnime = async (req, res) => {
 
   res.redirect("/admin/room");
 };
+
+async function getAnimeImage(title) {
+  try {
+    const response = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${encodeURIComponent(title)}`
+    );
+    const data = await response.json();
+    const anime = data.data[0];
+
+    if (!anime) return null;
+
+    return (
+      anime.images?.webp?.large_image_url ||
+      anime.images?.jpg?.large_image_url ||
+      null
+    );
+  } catch (error) {
+    console.error("Error fetching anime:", error);
+    return null;
+  }
+}
+
+function getAnimeLink(title) {
+  const kebabTitle = title.toLowerCase().replace(/\s+/g, "-");
+
+  const matchedLink = links.find((link) => link.includes(kebabTitle));
+  return matchedLink;
+}
